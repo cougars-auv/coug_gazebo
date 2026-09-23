e34cc91dd325d68285432391d27a83264a32764b
Nelson Durrant <snelsondurrant@gmail.com>
2026-09-23 00:11:34 -0600

    Add default mock

--- coug_gazebo/setup.py
+++ coug_gazebo/setup.py
@@ -13,7 +13,7 @@ setup(
         ("share/" + package_name, ["package.xml"]),
         (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
         (os.path.join("share", package_name, "launch"), glob("launch/*.launch.py")),
-        (os.path.join("share", package_name, "worlds"), glob("worlds/*.sdf.xacro")),
+        (os.path.join("share", package_name, "worlds"), glob("worlds/*.xacro")),
         *[
             (os.path.join("share", package_name, os.path.dirname(path)), [path])
             for path in glob("models/**/*.*", recursive=True)
